afe05dcffb4cd933356e87eca12f13b705330f60
dwelguisz <dwelguisz@indeed.com>
2021-12-05 21:41:05 -0600

    Code Clean up

--- src/com/dwelguisz/year2021/AdventDay06.java
+++ src/com/dwelguisz/year2021/AdventDay06.java
@@ -1,4 +1,19 @@
 package com.dwelguisz.year2021;
 
+import com.dwelguisz.year2021.helper.day5.Board;
+
+import java.util.List;
+
+import static com.dwelguisz.year2021.helper.ReadFile.readFile;
+
 public class AdventDay06 {
+
+    public static void main(String[] args) {
+        //List<String> lines = readFile("/home/dwelguisz/advent_of_coding/src/resources/year2021/day6/testcase.txt");
+        int part1 = 0;
+        int part2 = 1;
+        System.out.println(String.format("Part 1 Answer: %d", part1));
+        System.out.println(String.format("Part 2 Answer: %d", part2));
+    }
+
 }
