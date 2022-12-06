bf50606068c8e137bb02be58838933f2f476e7a7
David Welguisz <welguisz@gmail.com>
2022-12-06 10:26:03 -0600

    Change the for loop to a while loop to skip windows that will not work.

--- src/com/dwelguisz/year2022/TuningTrouble.java
+++ src/com/dwelguisz/year2022/TuningTrouble.java
@@ -1,6 +1,7 @@
 package com.dwelguisz.year2022;
 
 import com.dwelguisz.base.AoCDay;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,22 +21,24 @@ public class TuningTrouble extends AoCDay {
             values.add(i);
         }
         List<List<Integer>> combos = combinations(values, 2);
-        for (int i = 0; i < line.length()-size;i++) {
-            if (checkAllCombos(line.substring(i,i+size),combos)) {
-                return i + size;
+        Integer currentPosition = 0;
+        while (true) {
+            Pair<Boolean, Integer> result = checkAllCombos(line.substring(currentPosition, currentPosition+size), combos);
+            if (result.getLeft()) {
+                return currentPosition + size;
             }
+            currentPosition += result.getRight();
         }
-        return -1;
     }
 
 
-    public boolean checkAllCombos(String line, List<List<Integer>> combos) {
+    public Pair<Boolean,Integer> checkAllCombos(String line, List<List<Integer>> combos) {
         for (List<Integer> c : combos) {
             if (line.charAt(c.get(0)) == line.charAt(c.get(1))) {
-                return false;
+                return Pair.of(false, Integer.min(c.get(0), c.get(1))+1);
             }
         }
-        return true;
+        return Pair.of(true, -1);
     }
     public List<List<Integer>> combinations(List<Integer> inputSet, int k) {
         List<List<Integer>> results = new ArrayList<>();
