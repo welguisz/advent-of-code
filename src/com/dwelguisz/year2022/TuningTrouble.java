34e8ff742c43cedaa124988043db3c54f8b738ca
David Welguisz <welguisz@gmail.com>
2022-12-06 07:48:45 -0600

    Rewrote the two different parts into one part with a different size.

--- src/com/dwelguisz/year2022/TuningTrouble.java
+++ src/com/dwelguisz/year2022/TuningTrouble.java
@@ -8,36 +8,26 @@ import java.util.List;
 public class TuningTrouble extends AoCDay {
     public void solve() {
         List<String> lines = readFile("/Users/dwelguisz/personal/advent-of-code/src/resources/year2022/day06/input.txt");
-        Integer part1 = solutionPart1(lines.get(0));
+        Integer part1 = findPacket(lines.get(0),4);
         System.out.println(String.format("Part 1 Answer: %d",part1));
-        Integer part2 = solutionPart2(lines.get(0));
+        Integer part2 = findPacket(lines.get(0),14);
         System.out.println(String.format("Part 2 Answer: %d",part2));
     }
 
-    public Integer solutionPart1(String line) {
+    public Integer findPacket(String line, Integer size) {
         List<Integer> values = new ArrayList<>();
-        values.addAll(List.of(0,1,2,3));
+        for (int i = 0; i < size; i++) {
+            values.add(i);
+        }
         List<List<Integer>> combos = combinations(values, 2);
-        for (int i = 0; i < line.length()-4; i++) {
-            if (checkAllCombos(line.substring(i,i+4), combos)) {
-                return i + 4;
+        for (int i = 0; i < line.length()-size;i++) {
+            if (checkAllCombos(line.substring(i,i+size),combos)) {
+                return i + size;
             }
         }
         return -1;
     }
 
-    public Integer solutionPart2(String line) {
-        List<Integer> values = new ArrayList<>();
-        values.addAll(List.of(0,1,2,3,4,5,6,7,8,9));
-        values.addAll(List.of(10,11,12,13));
-        List<List<Integer>> combos = combinations(values, 2);
-        for(int i = 0; i < line.length() -14; i++) {
-            if (checkAllCombos(line.substring(i,i+14), combos)) {
-                return i + 14;
-            }
-        }
-        return -1;
-    }
 
     public boolean checkAllCombos(String line, List<List<Integer>> combos) {
         for (List<Integer> c : combos) {
