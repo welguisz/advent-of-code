607aae40218b0883274dbe4be373cbf2f400a4a0
David Welguisz <welguisz@gmail.com>
2022-12-15 12:12:29 -0600

    Year 2022. Day 15 completed.

--- src/com/dwelguisz/year2019/IntCodeComputer/IntCodeComputer.java
+++ src/com/dwelguisz/year2019/IntCodeComputer/IntCodeComputer.java
@@ -2,18 +2,16 @@ package com.dwelguisz.year2019.IntCodeComputer;
 
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.lang.reflect.Array;
 import java.util.*;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.stream.Collectors;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
 
 public class IntCodeComputer implements Runnable {
-    Long programCounter;
+    Long instructionPointer;
     Map<Long, Long> intCode;
     //Stores the opcodes and the jump for position
     Map<Long, Long> opCodes;
+    Map<Long, Integer> storeParamMap;
     ArrayDeque<Long> inputValues;
     ArrayDeque<Long> outputValues;
     Long id;
@@ -31,7 +29,7 @@ public class IntCodeComputer implements Runnable {
     Boolean stopOnFirstTime;
 
     public IntCodeComputer()  {
-        programCounter = 0L;
+        instructionPointer = 0L;
         id = 0L;
         relativeBaseAddress = 0L;
         done = false;
@@ -40,15 +38,8 @@ public class IntCodeComputer implements Runnable {
         outputValues = new ArrayDeque<>();
         debugValue = 0L;
         stopOnFirstTime = false;
-        opCodes.put(1L,4L);
-        opCodes.put(2L,4L);
-        opCodes.put(3L,2L);
-        opCodes.put(4L,2L);
-        opCodes.put(5L,3L);
-        opCodes.put(6L,3L);
-        opCodes.put(7L,4L);
-        opCodes.put(8L,4L);
-        opCodes.put(9L,2L);
+        opCodes = Map.of(1L,4L,2L,4L,3L,2L,4L,2L,5L,3L,6L,3L,7L,4L,8L,4L,9L,2L);
+        storeParamMap = Map.of(1L, 3, 2L, 3, 3L, 1, 4L, -1, 5L, 2, 6L, 2, 7L, 3, 8L, 3);
     }
 
     public void setInputQueue(ArrayDeque<Long> iqueue) {
@@ -125,88 +116,101 @@ public class IntCodeComputer implements Runnable {
         }
     }
 
-    public Long getMemoryAddress(Long offset, ParameterModes mode) {
-        if (mode == ParameterModes.immediateMode) {
-            return offset;
-        }
-        return (mode == ParameterModes.relativeMode) ? relativeBaseAddress + offset : offset;
-    }
-    public Long getMemoryValue(Long address, ParameterModes mode) {
-        if (mode == ParameterModes.immediateMode) {
-            return address;
-        }
-        return intCode.getOrDefault(address, 0L);
-    }
     public static Integer SLEEP_TIME = 10;
+
+    List<ParameterModes> findModes(Long opCode) {
+        List<ParameterModes> modes = new ArrayList<>();
+        Long divisor = 100L;
+        for (int i = 0; i < 3; i++) {
+            Long mode = (opCode / divisor) % 10L;
+            modes.add(updateMode(mode));
+            divisor *= 10L;
+        }
+        return modes;
+    }
+
+    Long getNewAddress(ParameterModes mode, Long value) {
+        if (mode == ParameterModes.positionMode) {
+            return value;
+        } else if (mode == ParameterModes.relativeMode) {
+            return relativeBaseAddress + value;
+        } else {
+            throw new RuntimeException("Unexpected Address mode: " + mode);
+        }
+    }
+
+    Long getOperand(ParameterModes mode, Long value) {
+        if (mode == ParameterModes.immediateMode) {
+            return value;
+        } else {
+            Long newAddress = getNewAddress(mode, value);
+            if (newAddress < 0) {
+                throw new ArrayIndexOutOfBoundsException("Address is negative: " +  newAddress);
+            }
+            return intCode.getOrDefault(newAddress, 0L);
+        }
+    }
+
+    void doCalculation(Long instr, List<ParameterModes> modes) {
+        if (instr.equals(1L)) {
+            Long value = TwoInputOperand(modes,(a,b) -> a+b);
+            Long address = getNewAddress(modes.get(2), intCode.getOrDefault(instructionPointer+3,0L));
+            intCode.put(address, value);
+            instructionPointer += opCodes.get(instr);
+        } else if (instr.equals(2L)) {
+            Long value = TwoInputOperand(modes,(a,b) -> a*b);
+            Long address = getNewAddress(modes.get(2), intCode.getOrDefault(instructionPointer+3,0L));
+            intCode.put(address, value);
+            instructionPointer += opCodes.get(instr);
+        } else if (instr.equals(3L)) {
+            Pair<Boolean, Long> res = getInputValue();
+            if (res.getLeft()) {
+                Long address = getNewAddress(modes.get(0), intCode.getOrDefault(instructionPointer+1,0L));
+                intCode.put(address, res.getRight());
+                instructionPointer += opCodes.get(instr);
+            } else {
+                try {
+                    Thread.sleep(SLEEP_TIME);
+                } catch (InterruptedException e) {
+
+                }
+            }
+        } else if (instr.equals(4L)) {
+            Long outputValue = getOperand(modes.get(0), instructionPointer + 1);
+            addOutputValue(outputValue);
+            debugValue = outputValue;
+            instructionPointer += opCodes.get(instr);
+        } else if (instr.equals(5L)) {
+            instructionPointer = TwoInputOperand(modes, (a,b) -> !a.equals(0L) ? b : instructionPointer + opCodes.get(instr));
+        } else if (instr.equals(6L)) {
+            instructionPointer = TwoInputOperand(modes, (a,b) -> a.equals(0L) ? b : instructionPointer + opCodes.get(instr));
+        } else if (instr.equals(7L)) {
+            Long result = TwoInputOperand(modes, (a,b) -> a < b ? 1L : 0L);
+            Long address = getNewAddress(modes.get(2), intCode.getOrDefault(instructionPointer+3,0L));
+            intCode.put(address, result);
+        } else if (instr.equals(8L)) {
+            Long result = TwoInputOperand(modes, (a,b) -> a.equals(b) ? 1L : 0L);
+            Long address = getNewAddress(modes.get(2), intCode.getOrDefault(instructionPointer+3,0L));
+            intCode.put(address, result);
+        } else if (instr.equals(9L)) {
+            relativeBaseAddress += getOperand(modes.get(0),instructionPointer+1);
+        }
+    }
+
+    Long TwoInputOperand(List<ParameterModes> modes, BinaryOperator<Long> func) {
+        Long operand1 = getOperand(modes.get(0), instructionPointer+1);
+        Long operand2 = getOperand(modes.get(1), instructionPointer+2);
+        return func.apply(operand1, operand2);
+    }
+
     public void run() {
-        Long currentInstructionWithMode = intCode.getOrDefault(programCounter,0L);
+        Long currentInstructionWithMode = intCode.getOrDefault(instructionPointer,0L);
         Long currentInstruction = currentInstructionWithMode % 100;
-        boolean incProgramCounter = true;
         done = false;
         while ((currentInstruction != 99) && (opCodes.containsKey(currentInstruction))) {
-            incProgramCounter = true;
-            Long aMode = (currentInstructionWithMode / 100L) % 10L;
-            Long bMode = (currentInstructionWithMode / 1000L) % 10L;
-            Long cMode = (currentInstructionWithMode / 10000L) % 10L;
-            ParameterModes mode0 = updateMode(aMode);
-            ParameterModes mode1 = updateMode(bMode);
-            ParameterModes mode2 = updateMode(cMode);
-            Long opPointer1 = intCode.getOrDefault(programCounter + 1, 0L);
-            Long opPointer2 = intCode.getOrDefault(programCounter + 2, 0L);
-            Long storePointer = intCode.getOrDefault(programCounter + 3, 0L);
-            opPointer1 = getMemoryAddress(opPointer1, mode0);
-            opPointer2 = getMemoryAddress(opPointer2, mode1);
-            storePointer = getMemoryAddress(storePointer, mode2);
-            Long value1 = getMemoryValue(opPointer1, mode0);
-            Long value2 = getMemoryValue(opPointer2, mode1);
-            if (currentInstruction.equals(1L)) {
-                intCode.put(storePointer, value1 + value2);
-            } else if (currentInstruction.equals(2L)) {
-                intCode.put(storePointer, value1 * value2);
-            } else if (currentInstruction.equals(3L)) {
-                Pair<Boolean, Long> value = getInputValue();
-                if (value.getLeft()) {
-                    Long address = getMemoryAddress(opPointer1, mode0);
-                    intCode.put(address, value.getRight());
-                } else {
-                    programCounter -= opCodes.get(3L);
-                    try {
-                        Thread.sleep(SLEEP_TIME);
-                    } catch (InterruptedException e) {
-
-                    }
-                }
-            } else if (currentInstruction.equals(4L)) {
-                Long outputValue = getMemoryValue(opPointer1, mode0);
-                addOutputValue(outputValue);
-                debugValue = outputValue;
-            } else if (currentInstruction.equals(5L)) {
-                if (!value1.equals(0L)) {
-                    programCounter = value2;
-                    incProgramCounter = false;
-                }
-            } else if (currentInstruction.equals(6L)) {
-                if (value1.equals(0L)) {
-                    programCounter = value2;
-                    incProgramCounter = false;
-                }
-            } else if (currentInstruction.equals(7L)) {
-                Long value = 0L;
-                if (value1 < value2) {
-                    value = 1L;
-                }
-                intCode.put(storePointer, value);
-            } else if (currentInstruction.equals(8L)) {
-                Long value = 0L;
-                if (value1.equals(value2)) {
-                    value = 1L;
-                }
-                intCode.put(storePointer, value);
-            } else if (currentInstruction.equals(9L)) {
-                relativeBaseAddress += opPointer1;
-            }
-            programCounter += incProgramCounter? opCodes.get(currentInstruction) : 0;
-            currentInstructionWithMode = intCode.getOrDefault(programCounter, 0L);
+            List<ParameterModes> modes = findModes(currentInstructionWithMode);
+            doCalculation(currentInstruction, modes);
+            currentInstructionWithMode = intCode.getOrDefault(instructionPointer, 0L);
             currentInstruction = currentInstructionWithMode % 100;
         }
         done = true;
---
+++ src/com/dwelguisz/year2022/BeaconExclusionZone.java
@@ -0,0 +1,118 @@
+package com.dwelguisz.year2022;
+
+import com.dwelguisz.base.AoCDay;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BeaconExclusionZone extends AoCDay {
+
+    public static class Sensor {
+        Integer id;
+        Integer bx;
+        Integer by;
+        Integer sx;
+        Integer sy;
+        Integer md;
+
+        public Sensor(Integer id, Integer sx, Integer sy, Integer bx, Integer by) {
+            this.id = id;
+            this.sx = sx;
+            this.sy = sy;
+            this.bx = bx;
+            this.by = by;
+            this.md = manhattanDistance(bx,by,sx,sy);
+        }
+
+        public List<Integer> getKnownRange(Integer y) {
+            if (Math.abs(y - sy) < md) {
+                Integer k = md - Math.abs(sy-y);
+                return List.of(sx -k, sx+k);
+            }
+            return null;
+        }
+        public Integer manhattanDistance(Integer cx, Integer cy, Integer ax, Integer ay) {
+            return Math.abs(cx-ax) + Math.abs(cy-ay);
+        }
+
+    }
+
+
+    public void solve() {
+        List<String> lines = readFile("/Users/dwelguisz/personal/advent-of-code/src/resources/year2022/day15/input.txt");
+        Long parseTime = Instant.now().toEpochMilli();
+        List<Sensor> sensors = parseLines(lines);
+        Long startTime = Instant.now().toEpochMilli();
+        Integer part1 = solutionPart1(sensors, 2000000);
+        Long part1Time = Instant.now().toEpochMilli();
+        Long part2 = solutionPart2(sensors);
+        Long part2Time = Instant.now().toEpochMilli();
+        System.out.println(String.format("Part 1 Answer: %d",part1));
+        System.out.println(String.format("Part 2 Answer: %d",part2));
+        System.out.println(String.format("Parsing Time: %d ms.", startTime - parseTime));
+        System.out.println(String.format("Time to do Part 1: %d ms.", part1Time - startTime));
+        System.out.println(String.format("Time to do Part 2: %d ms.", part2Time - part1Time));
+    }
+
+    public List<Sensor> parseLines(List<String> lines) {
+        List<Sensor> sensors = new ArrayList<>();
+        Integer id = 0;
+        for (String line : lines) {
+            String sp[] = line.split(" ");
+            Integer sx = Integer.parseInt(sp[2].substring(2,sp[2].length()-1));
+            Integer sy = Integer.parseInt(sp[3].substring(2,sp[3].length()-1));
+            Integer bx = Integer.parseInt(sp[8].substring(2, sp[8].length()-1));
+            Integer by = Integer.parseInt(sp[9].substring(2));
+            sensors.add(new Sensor(id,sx,sy,bx,by));
+            id++;
+        }
+        return sensors;
+    }
+
+    public Integer solutionPart1(List<Sensor> sensors, Integer line) {
+        List<List<Integer>> sensorRanges = sensors.stream().map(s -> s.getKnownRange(line)).filter(s -> s != null).collect(Collectors.toList());
+        sensorRanges = mergeIntervals(sensorRanges);
+        return sensorRanges.get(0).get(1)-sensorRanges.get(0).get(0);
+    }
+
+    public Integer comparePairs(List<Integer> a, List<Integer> b) {
+        return a.get(0) - b.get(0);
+    }
+
+    public List<List<Integer>> mergeIntervals(List<List<Integer>> intervals) {
+        if (intervals.isEmpty()) {
+            return new ArrayList<>();
+        }
+        intervals = intervals.stream().sorted((a,b) -> comparePairs(a,b)).collect(Collectors.toList());
+        int i = 0;
+        while (i < intervals.size()-1) {
+            if (intervals.get(i).get(1)+1 >= intervals.get(i+1).get(0)) {
+                Integer maxV = Integer.max(intervals.get(i).get(1), intervals.get(i+1).get(1));
+                List<Integer> tmp = List.of(intervals.get(i).get(0), maxV);
+                intervals.remove(i+1);
+                intervals.remove(i);
+                intervals.add(i,tmp);
+                continue;
+            }
+            i++;
+        }
+        return intervals;
+    }
+
+    public Long solutionPart2(List<Sensor> sensors) {
+        for(Integer i = 0; i < 4000000; i++) {
+            final Integer line = i;
+            List<List<Integer>> ranges = sensors.stream().map(s -> s.getKnownRange(line)).filter(s -> s != null).collect(Collectors.toList());
+            ranges = mergeIntervals(ranges);
+            if (ranges.size() > 1) {
+                Integer hole = ranges.get(0).get(1) + 1;
+                System.out.println("Beacon is at (" + hole + ","+i+")");
+                Long val = 4000000L * hole;
+                return val + i;
+            }
+        }
+        return -1L;
+    }
+}
