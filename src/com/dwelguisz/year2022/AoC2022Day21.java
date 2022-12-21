f90c5d96c2fde49b39fef26285f2dbeebc643f57
David Welguisz <welguisz@gmail.com>
2022-12-21 01:20:44 -0600

    Off by one bug.  Trying to find the source.  tmp is coming soon

--- src/com/dwelguisz/year2022/AoC2022Day21.java
+++ src/com/dwelguisz/year2022/AoC2022Day21.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Stack;
 import java.util.regex.Pattern;
 
 public class AoC2022Day21 extends AoCDay {
@@ -17,12 +16,16 @@ public class AoC2022Day21 extends AoCDay {
         Long parseTime = Instant.now().toEpochMilli();
         Map<String, String> values = parsedLines(lines);
         Long startTime = Instant.now().toEpochMilli();
-        Long part1 = solutionPart1(new HashMap<>(values), "root");
+        Long part1Iterative = solutionPart1(values, false);
+        Long part1Recursion = solutionPart1(values, true);
         Long part1Time = Instant.now().toEpochMilli();
-        Long part2 = solutionPart2(new HashMap<>(values));
+        Long part2Iterative = solutionPart2(values, false);
+        Long part2Recursion = solutionPart2(values, true);
         Long part2Time = Instant.now().toEpochMilli();
-        System.out.println(String.format("Part 1 Answer: %d",part1));
-        System.out.println(String.format("Part 2 Answer: %d",part2));
+        System.out.println(String.format("Part 1 Iterative Answer: %d",part1Iterative));
+        System.out.println(String.format("Part 2 Recursion Answer: %d",part1Recursion));
+        System.out.println(String.format("Part 2 Iterative Answer: %d",part2Iterative));
+        System.out.println(String.format("Part 2 Recursion Answer: %d",part2Recursion));
         System.out.println(String.format("Parsing Time: %d ms.", startTime - parseTime));
         System.out.println(String.format("Time to do Part 1: %d ms.", part1Time - startTime));
         System.out.println(String.format("Time to do Part 2: %d ms.", part2Time - part1Time));
@@ -46,7 +49,7 @@ public class AoC2022Day21 extends AoCDay {
         return pattern.matcher(strNum).matches();
     }
 
-    public Long monkeyOperation(Map<String, String> monkeys, String name, Long human) {
+    public Long monkeyOperationRecursion(Map<String, String> monkeys, String name, Long human) {
         String monkey = monkeys.get(name);
         if (name.equals("humn") && human >= 0) {
             return human;
@@ -57,8 +60,8 @@ public class AoC2022Day21 extends AoCDay {
         String operand1Str = monkey.substring(0,4);
         String operand2Str = monkey.substring(7);
         Character op = monkey.substring(5,6).toCharArray()[0];
-        Long operand1 = monkeyOperation(monkeys,operand1Str,human);
-        Long operand2 = monkeyOperation(monkeys,operand2Str,human);
+        Long operand1 = monkeyOperationRecursion(monkeys,operand1Str,human);
+        Long operand2 = monkeyOperationRecursion(monkeys,operand2Str,human);
         switch (op) {
             case '+':
                 return operand1 + operand2;
@@ -72,8 +75,12 @@ public class AoC2022Day21 extends AoCDay {
         return 0L;
     }
 
-    public Long solutionPart1(Map<String, String> values, String keyToFind) {
+    public Long monkeyOperation(Map<String, String> values, String keyToFind, Long humanValue) {
         Map<String, Long> decoded = new HashMap<>();
+        if (humanValue >= 0) {
+            decoded.put("humn",humanValue);
+            values.remove("humn");
+        }
         List<String> removeKeys = new ArrayList<>();
         for (Map.Entry<String, String>v : values.entrySet()) {
             if (isNumeric(v.getValue())) {
@@ -100,24 +107,36 @@ public class AoC2022Day21 extends AoCDay {
             }
         }
         return decoded.get(keyToFind);
+
+    }
+    public Long solutionPart1(Map<String, String> values, boolean recursion) {
+        return useIterativeOrRecursion(new HashMap<>(values), "root", -1L, recursion);
     }
 
-    public Long solutionPart2(Map<String, String> values) {
+    public Long useIterativeOrRecursion(Map<String,String> values, String keyToFind, Long humanValue, Boolean recursive) {
+        if (recursive) {
+            return monkeyOperationRecursion(new HashMap<>(values), keyToFind, humanValue);
+        }
+        return monkeyOperation(new HashMap<>(values), keyToFind, humanValue);
+    }
+
+    public Long solutionPart2(Map<String, String> values, Boolean recursive) {
         String rootExpr = values.get("root");
         String root1Str = rootExpr.substring(0,4);
         String root2Str = rootExpr.substring(7);
-        if (!monkeyOperation(values, root2Str, 0L).equals(monkeyOperation(values, root2Str, 1L))) {
+        if (!useIterativeOrRecursion(values, root2Str, 0L, recursive).equals(useIterativeOrRecursion(values, root2Str, 1L, recursive))) {
             String tmp = root1Str;
             root1Str = root2Str;
             root2Str = tmp;
         }
-        Long target = monkeyOperation(values, root2Str, 0L);
+        Long target = useIterativeOrRecursion(values, root2Str, 0L, recursive);
 
         Long low = 0L;
         Long high = Long.MAX_VALUE;
         while (low < high) {
             Long mid = (low + high) / 2;
-            Long score = target - monkeyOperation(values, root1Str, mid);
+            Long tmp = useIterativeOrRecursion(values, root1Str, mid, recursive);
+            Long score = target - tmp;
             if (score < 0) {
                 low = mid;
             } else if (score == 0) {
