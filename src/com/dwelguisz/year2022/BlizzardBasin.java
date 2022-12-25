88c8e0e34b8e6c531afb2f6c0be1d44a13fe7d19
David Welguisz <welguisz@gmail.com>
2022-12-24 23:49:26 -0600

    Clean-up on Day 24.

--- src/com/dwelguisz/year2022/BlizzardBasin.java
+++ src/com/dwelguisz/year2022/BlizzardBasin.java
@@ -33,12 +33,20 @@ public class BlizzardBasin extends AoCDay {
         System.out.println(String.format("Time to do Part 2: %d ms.", part2Time - part1Time));
     }
 
-    Set<Coord2D> calculateBlizzards(Integer time) {
+    public Integer modulo(Integer a, Integer b) {
+        return (a >= 0) ? a % b : b - (Math.abs(a) % b);
+    }
+
+    Set<Coord2D> findFreeSpaces(Integer time) {
+        Set<Coord2D> spaces = new HashSet<>();
         Integer yModulo = map.length - 2;
         Integer xModulo = map[0].length - 2;
         Set<Coord2D> blizzards = new HashSet<>();
-        for(int y = 0; y < map.length; y++) {
+        for (int y = 0; y < map.length; y++) {
             for (int x = 0; x < map[0].length;x++) {
+                if (!map[y][x].equals("#")) {
+                    spaces.add(new Coord2D(y, x));
+                }
                 if (map[y][x].equals("^")) {
                     blizzards.add(new Coord2D(modulo(y-time-1,yModulo)+1,x));
                 } else if (map[y][x].equals("v")) {
@@ -48,23 +56,6 @@ public class BlizzardBasin extends AoCDay {
                 } else if (map[y][x].equals(">")) {
                     blizzards.add(new Coord2D(y,modulo(x+time-1,xModulo)+1));
                 }
-            }
-        }
-        return blizzards;
-    }
-
-    public Integer modulo(Integer a, Integer b) {
-        return (a >= 0) ? a % b : b - (Math.abs(a) % b);
-    }
-
-    Set<Coord2D> findFreeSpaces(Set<Coord2D> blizzards) {
-        Set<Coord2D> spaces = new HashSet<>();
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length;x++) {
-                if (map[y][x].equals("#")) {
-                    continue;
-                }
-                spaces.add(new Coord2D(y,x));
             }
         }
         spaces.removeAll(blizzards);
@@ -81,8 +72,7 @@ public class BlizzardBasin extends AoCDay {
         //a sequence of maps that are available during each time period.
         int maxBlizzards = (lines.size()-2) * (lines.get(0).length()-2);
         for (int i = 0; i < maxBlizzards; i++) {
-            Set<Coord2D> blizzardAtTimeI = calculateBlizzards(i);
-            freePath.add(findFreeSpaces(blizzardAtTimeI));
+            freePath.add(findFreeSpaces(i));
         }
     }
 
@@ -90,15 +80,15 @@ public class BlizzardBasin extends AoCDay {
     Integer solutionPart1() {
         Coord2D startingPoint = new Coord2D(0,1);
         Coord2D endingPoint = new Coord2D(map.length-1,map[0].length-2);
-        return findShortestPath(300, 0,startingPoint, endingPoint);
+        return findShortestPath(0,startingPoint, endingPoint);
     }
 
     Integer solutionPart2() {
         Coord2D startingPoint = new Coord2D(0,1);
         Coord2D endingPoint = new Coord2D(map.length-1,map[0].length-2);
-        Integer time =  findShortestPath(300, 0, startingPoint, endingPoint);
-        time = findShortestPath(600,time+1, endingPoint, startingPoint);
-        return findShortestPath(900,time+1,startingPoint, endingPoint);
+        Integer time =  findShortestPath(0, startingPoint, endingPoint);
+        time = findShortestPath(time+1, endingPoint, startingPoint);
+        return findShortestPath(time+1,startingPoint, endingPoint);
     }
 
     Stream<Coord2D> nextSpots(Coord2D loc, Set<Coord2D> freeSpace) {
@@ -112,21 +102,19 @@ public class BlizzardBasin extends AoCDay {
 
     }
     public Integer findShortestPath(
-            Integer maxSteps,
             Integer initialTime,
             Coord2D startingLocation,
             Coord2D endingLocation
     ) {
         Set<Coord2D> states = new HashSet<>();
         states.add(startingLocation);
-        for (Integer i = initialTime; i < maxSteps; i++) {
+        for (Integer i = initialTime; true; i++) {
             Set<Coord2D> openSpaces = freePath.get((i +1 )% freePath.size());
             states = states.stream().flatMap(s -> nextSpots(s,openSpaces)).collect(Collectors.toSet());
             if (states.stream().anyMatch(s -> s.equals(endingLocation))) {
                 return i+1;
             }
         }
-        return Integer.MAX_VALUE;
     }
 
 
