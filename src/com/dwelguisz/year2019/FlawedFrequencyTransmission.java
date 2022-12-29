bc40f4c3396b10c5f35811f5367fa187eac90b75
David Welguisz <welguisz@gmail.com>
2022-12-28 22:51:14 -0600

    Completed Year 2019, Day 16. Nice little trick there to see what was important and what was not important.

---
+++ src/com/dwelguisz/year2019/FlawedFrequencyTransmission.java
@@ -0,0 +1,88 @@
+package com.dwelguisz.year2019;
+
+import com.dwelguisz.base.AoCDay;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FlawedFrequencyTransmission extends AoCDay {
+
+    public static List<Integer> BASE_PATTERN = List.of(0,1,0,-1);
+
+    public void solve() {
+        List<String> lines = readFile("/Users/dwelguisz/personal/advent-of-code/src/resources/year2019/day16/input.txt");
+        Long parseTime = Instant.now().toEpochMilli();
+        List<Integer> values = parseLines(lines.get(0));
+        Long startTime = Instant.now().toEpochMilli();
+        String part1 = solutionPart1(values);
+        Long part1Time = Instant.now().toEpochMilli();
+        String part2 = solutionPart2(values);
+        Long part2Time = Instant.now().toEpochMilli();
+        System.out.println(String.format("Parsing Time: %d ms.", startTime - parseTime));
+        System.out.println(String.format("Part 1 Answer: %s",part1));
+        System.out.println(String.format("Time to do Part 1: %d ms.", part1Time - startTime));
+        System.out.println(String.format("Part 2 Answer: %s",part2));
+        System.out.println(String.format("Time to do Part 2: %d ms.", part2Time - part1Time));
+    }
+
+    public List<Integer> parseLines (String line) {
+        return Arrays.stream(line.split("")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    public String solutionPart1(List<Integer> values) {
+        for (int j = 0; j < 100; j++) {
+            List<Integer> newValues = new ArrayList<>();
+            for (int i = 0; i < values.size(); i++) {
+                newValues.add(runOnePattern(values, createPattern(i)));
+            }
+            values = newValues;
+        }
+        return values.subList(0,8).stream().map(i -> i.toString()).collect(Collectors.joining());
+    }
+
+    public Integer runOnePattern(final List<Integer> values, final List<Integer> pattern) {
+        Integer patternSize = pattern.size();
+        return (Math.abs(IntStream.range(0,values.size()).boxed().mapToInt(j -> values.get(j) * pattern.get((j+1)%patternSize)).sum())%10);
+    }
+
+    public String solutionPart2(List<Integer> values) {
+        Integer offset = 0;
+        for (int i = 0; i < 7; i++) {
+            offset *= 10;
+            offset += values.get(i);
+        }
+        Integer totalElements = values.size() * 10000;
+        List<Integer> trueFFT = new ArrayList<>();
+        for (int i = totalElements-1; i >= offset; i--) {
+            trueFFT.add(values.get(i % values.size()));
+        }
+        for (int j = 0; j < 100; j++) {
+            List<Integer> newValues = new ArrayList<>();
+            Integer runningSum = 0;
+            for (int i = 0; i < trueFFT.size(); i++) {
+                runningSum = (runningSum + trueFFT.get(i)) % 10;
+                newValues.add(runningSum);
+            }
+            trueFFT = newValues;
+        }
+        List<String> finalValues = trueFFT.stream().map(i -> i.toString()).collect(Collectors.toList());
+        Collections.reverse(finalValues);
+        return finalValues.subList(0,8).stream().collect(Collectors.joining());
+    }
+
+    public List<Integer> createPattern(Integer elementNumber) {
+        List<Integer> pattern = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= elementNumber; j++) {
+                pattern.add(BASE_PATTERN.get(i));
+            }
+        }
+        return pattern;
+    }
+
+}
