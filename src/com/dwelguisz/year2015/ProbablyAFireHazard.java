dbd4e90e174cbc7de6508dbd09ccc1e9f6e13d6e
David Welguisz <welguisz@gmail.com>
2023-01-24 20:42:24 -0600

    Cleaned up Year 2015, Day 6.

--- src/com/dwelguisz/year2015/ProbablyAFireHazard.java
+++ src/com/dwelguisz/year2015/ProbablyAFireHazard.java
@@ -1,112 +1,119 @@
 package com.dwelguisz.year2015;
 
 import com.dwelguisz.base.AoCDay;
+import com.dwelguisz.utilities.Coord2D;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.lang.Integer.parseInt;
 
 public class ProbablyAFireHazard extends AoCDay {
-    static int MAX_ROW = 1000;
-    static Boolean[][] grid = new Boolean[MAX_ROW][MAX_ROW];
-    static Integer[][] betterGrid = new Integer[MAX_ROW][MAX_ROW];
-    static {
-        for (int i = 0; i < MAX_ROW; i++) {
-            for (int j = 0; j < MAX_ROW; j++) {
-                grid[i][j] = false;
-                betterGrid[i][j] = 0;
+
+    public static class Instruction {
+        Boolean toggle;
+        Boolean turnOn;
+        Coord2D startingPoint;
+        Coord2D endingPoint;
+
+        public Instruction(Boolean toggle, Boolean turnOn, Coord2D startingPoint, Coord2D endingPoint) {
+            this.toggle = toggle;
+            this.turnOn = turnOn;
+            this.startingPoint = startingPoint;
+            this.endingPoint = endingPoint;
+        }
+
+        List<Coord2D> pointsToGet() {
+            List<Coord2D> points = new ArrayList<>();
+            for (Integer x = startingPoint.x; x <= endingPoint.x; x++) {
+                for (Integer y = startingPoint.y; y <= endingPoint.y; y++) {
+                    points.add(new Coord2D(x,y));
+                }
             }
+            return points;
         }
     }
 
     public void solve() {
-        List<String> instructions = readFile("/home/dwelguisz/advent_of_code/src/resources/year2015/day06/input.txt");
-        Long part1 = solutionPart1(instructions);
-        Long part2 = solutionPart2(instructions);
-        System.out.println(String.format("Part 1 Answer: %d", part1));
-        System.out.println(String.format("Part 2 Answer: %d", part2));
+        timeMarkers[0] = Instant.now().toEpochMilli();
+        List<String> lines = readResoruceFile(2015,6,false,0);
+        List<Instruction> instructions = parseLines(lines);
+        timeMarkers[1] = Instant.now().toEpochMilli();
+        part1Answer = solutionPart1(instructions);
+        timeMarkers[2] = Instant.now().toEpochMilli();
+        part2Answer = solutionPart2(instructions);
+        timeMarkers[3] = Instant.now().toEpochMilli();
     }
 
-    static Long solutionPart1(List<String> strings) {
-        for (String str : strings) {
-            List<String> instruction = Arrays.asList(str.split(" "));
-            if (instruction.get(0).equals("turn")) {
-                String point1 = instruction.get(2);
-                String point2 = instruction.get(4);
+    List<Instruction> parseLines(List<String> lines) {
+        List<Instruction> instructions = new ArrayList<>();
+        for (String l : lines) {
+            String split[] = l.split(" ");
+            if (split[0].equals("turn")) {
+                String point1 = split[2];
+                String point2 = split[4];
                 List<Integer> point1I = Arrays.stream(point1.split(",")).map(str1 -> parseInt(str1)).collect(Collectors.toList());
                 List<Integer> point2I = Arrays.stream(point2.split(",")).map(str1 -> parseInt(str1)).collect(Collectors.toList());
-                for (int x = point1I.get(0); x <= point2I.get(0); x++) {
-                    for (int y = point1I.get(1); y <= point2I.get(1); y++) {
-                        if (instruction.get(1).equals("off")) {
-                            grid[x][y] = false;
-                        } else if (instruction.get(1).equals("on")) {
-                            grid[x][y] = true;
-                        }
-                    }
-                }
-            } else if (instruction.get(0).equals("toggle")) {
-                String point1 = instruction.get(1);
-                String point2 = instruction.get(3);
+                Coord2D startingPoint = new Coord2D(point1I.get(0), point1I.get(1));
+                Coord2D endingPoint = new Coord2D(point2I.get(0), point2I.get(1));
+                Boolean position = split[1].equals("on");
+                instructions.add(new Instruction(false, position, startingPoint, endingPoint));
+            } else {
+                String point1 = split[1];
+                String point2 = split[3];
                 List<Integer> point1I = Arrays.stream(point1.split(",")).map(str1 -> parseInt(str1)).collect(Collectors.toList());
                 List<Integer> point2I = Arrays.stream(point2.split(",")).map(str1 -> parseInt(str1)).collect(Collectors.toList());
-                for (int x = point1I.get(0); x <= point2I.get(0); x++) {
-                    for (int y = point1I.get(1); y <= point2I.get(1); y++) {
-                        grid[x][y] ^= true;
+                Coord2D startingPoint = new Coord2D(point1I.get(0), point1I.get(1));
+                Coord2D endingPoint = new Coord2D(point2I.get(0), point2I.get(1));
+                instructions.add(new Instruction(true,false,startingPoint,endingPoint));
+            }
+        }
+        return instructions;
+    }
+    Long solutionPart1(List<Instruction> instructions) {
+        Map<Coord2D, Boolean> map = new HashMap<>();
+        for (Instruction instruction : instructions) {
+            List<Coord2D> points = instruction.pointsToGet();
+            for (Coord2D point : points) {
+                Boolean value = map.getOrDefault(point, false);
+                if (instruction.toggle) {
+                    value ^= true;
+                } else {
+                    if (instruction.turnOn) {
+                        value = true;
+                    } else {
+                        value = false;
                     }
                 }
+                map.put(point, value);
             }
         }
-        Long count = 0L;
-        for (int x = 0; x < MAX_ROW; x++) {
-            for (int y = 0; y < MAX_ROW; y++) {
-                if (grid[x][y]) {
-                    count++;
-                }
-            }
-        }
-        return count;
+        return map.entrySet().stream().filter(e -> e.getValue()).count();
     }
 
-    static Long solutionPart2(List<String> strings) {
-        for (String str : strings) {
-            List<String> instruction = Arrays.asList(str.split(" "));
-            if (instruction.get(0).equals("turn")) {
-                String point1 = instruction.get(2);
-                String point2 = instruction.get(4);
-                List<Integer> point1I = Arrays.stream(point1.split(",")).map(str1 -> parseInt(str1)).collect(Collectors.toList());
-                List<Integer> point2I = Arrays.stream(point2.split(",")).map(str1 -> parseInt(str1)).collect(Collectors.toList());
-                for (int x = point1I.get(0); x <= point2I.get(0); x++) {
-                    for (int y = point1I.get(1); y <= point2I.get(1); y++) {
-                        if (instruction.get(1).equals("off")) {
-                            betterGrid[x][y] -= 1;
-                            if (betterGrid[x][y] < 0) {
-                                betterGrid[x][y] = 0;
-                            }
-                        } else if (instruction.get(1).equals("on")) {
-                            betterGrid[x][y] += 1;
-                        }
+    Integer solutionPart2(List<Instruction> instructions) {
+        Map<Coord2D, Integer> map = new HashMap<>();
+        for (Instruction instruction : instructions) {
+            List<Coord2D> points = instruction.pointsToGet();
+            for (Coord2D point : points) {
+                Integer value = map.getOrDefault(point, 0);
+                if (instruction.toggle) {
+                    value += 2;
+                } else {
+                    if (instruction.turnOn) {
+                        value += 1;
+                    } else {
+                        value -= (value == 0) ? 0 : 1;
                     }
                 }
-            } else if (instruction.get(0).equals("toggle")) {
-                String point1 = instruction.get(1);
-                String point2 = instruction.get(3);
-                List<Integer> point1I = Arrays.stream(point1.split(",")).map(str1 -> parseInt(str1)).collect(Collectors.toList());
-                List<Integer> point2I = Arrays.stream(point2.split(",")).map(str1 -> parseInt(str1)).collect(Collectors.toList());
-                for (int x = point1I.get(0); x <= point2I.get(0); x++) {
-                    for (int y = point1I.get(1); y <= point2I.get(1); y++) {
-                        betterGrid[x][y] += 2;
-                    }
-                }
+                map.put(point, value);
             }
         }
-        Long count = 0L;
-        for (int x = 0; x < MAX_ROW; x++) {
-            for (int y = 0; y < MAX_ROW; y++) {
-                count += betterGrid[x][y];
-            }
-        }
-        return count;
+        return map.entrySet().stream().mapToInt(e -> e.getValue()).sum();
     }
 }
---
+++ src/resources/year2015/day06/README.md
@@ -0,0 +1,74 @@
+# Day 6: Probably a Fire Hazard
+
+[Back to Top README file](../../../../README.md)
+## Overview
+Difficult Level: Easy/Medium
+
+Input: List of instructions to control lights
+
+## Parsing
+For the parsing portion, I created a class that holds the following information:
+* Toggle
+* Turn On
+* Starting Coordinate
+* Ending Coordinate
+
+This class also includes a function that will return all points from the upper left
+corner of the square (starting coordinate) to the lower right corner of the 
+square (ending coordinate).
+
+## Part 1
+Create a Map that has a Key of the `Coord2D` and value of `Boolean`. Depending on
+the instruction, change the value to `true`, `false`, or flip the value.
+
+```java
+    Long solutionPart1(List<Instruction> instructions) {
+        Map<Coord2D, Boolean> map = new HashMap<>();
+        for (Instruction instruction : instructions) {
+            List<Coord2D> points = instruction.pointsToGet();
+            for (Coord2D point : points) {
+                Boolean value = map.getOrDefault(point, false);
+                if (instruction.toggle) {
+                    value ^= true;
+                } else {
+                    if (instruction.turnOn) {
+                        value = true;
+                    } else {
+                        value = false;
+                    }
+                }
+                map.put(point, value);
+            }
+        }
+        return map.entrySet().stream().filter(e -> e.getValue()).count();
+    }
+```
+
+## Part 2
+Similar to Part 1, but instead of Boolean, use Integer. The brightness of the lights
+can't be negative.
+
+```java
+    Integer solutionPart2(List<Instruction> instructions) {
+        Map<Coord2D, Integer> map = new HashMap<>();
+        for (Instruction instruction : instructions) {
+            List<Coord2D> points = instruction.pointsToGet();
+            for (Coord2D point : points) {
+                Integer value = map.getOrDefault(point, 0);
+                if (instruction.toggle) {
+                    value += 2;
+                } else {
+                    if (instruction.turnOn) {
+                        value += 1;
+                    } else {
+                        value -= (value == 0) ? 0 : 1;
+                    }
+                }
+                map.put(point, value);
+            }
+        }
+        return map.entrySet().stream().mapToInt(e -> e.getValue()).sum();
+    }
+```
+
+|[Previous (Day 5)](../day05/README.md)|[Next (Day 7)](../day07/README.md)|
