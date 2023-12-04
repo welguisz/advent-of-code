c2e2dfa841d8e332a9fb760eea80cfe14d3d2a08
David Welguisz <welguisz@gmail.com>
2023-12-04 00:09:23 -0600

    More clean-up

--- src/com/dwelguisz/year2023/Scratchcards.java
+++ src/com/dwelguisz/year2023/Scratchcards.java
@@ -13,38 +13,41 @@ import java.util.stream.Collectors;
 
 public class Scratchcards extends AoCDay {
 
-    List<Integer> winningSizePerCard;
     public void solve() {
-        this.winningSizePerCard = new ArrayList<>();
         timeMarkers[0] = Instant.now().toEpochMilli();
         List<String> lines = readResoruceFile(2023, 4, false, 0);
+        List<Integer> winningNumsPerCard = parseLines(lines);
         timeMarkers[1] = Instant.now().toEpochMilli();
-        part1Answer = solutionPart1(lines);
+        part1Answer = solutionPart1(winningNumsPerCard);
         timeMarkers[2] = Instant.now().toEpochMilli();
-        part2Answer = solutionPart2();
+        part2Answer = solutionPart2(winningNumsPerCard);
         timeMarkers[3] = Instant.now().toEpochMilli();
     }
 
     private Set<Integer> createSet(String val) {
         return Arrays.stream(val.split(("\\s+"))).filter(s -> s.length() > 0).map(Integer::parseInt).collect(Collectors.toSet());
     }
-    private Long solutionPart1(List<String> lines) {
-        Long total = 0L;
-        for (String line : lines) {
-            // We don't care about the Card #: portion, so just ignore it.
-            String goodPart = line.split(":\\s+")[1];
-            String [] parts = goodPart.split(" \\| ");
-            Set<Integer> winningNumbers = createSet(parts[0]);
-            Set<Integer> myNumbers = createSet(parts[1]);
-            myNumbers.retainAll(winningNumbers);
-            winningSizePerCard.add(myNumbers.size());
-            Long val = (long) Math.pow(2, myNumbers.size()-1);
-            total += val;
-        }
-        return total;
+
+    private List<Integer> parseLines(List<String> lines) {
+        return lines.stream()
+                .map(l -> l.split(":\\s+")[1])
+                .map(l -> {
+                    String [] parts = l.split(" \\| ");
+                    Set<Integer> winningNumbers = createSet(parts[0]);
+                    Set<Integer> myNumbers = createSet(parts[1]);
+                    myNumbers.retainAll(winningNumbers);
+                    return myNumbers.size();
+                })
+                .collect(Collectors.toList());
+    }
+    private Long solutionPart1(List<Integer> winningSizePerCard) {
+        return winningSizePerCard.stream()
+                .filter(i -> i > 0)
+                .mapToLong(i -> (long) Math.pow(2,i-1))
+                .sum();
     }
 
-    private Long solutionPart2() {
+    private Long solutionPart2(List<Integer> winningSizePerCard) {
         Map<Integer, Integer> winners = new HashMap<>();
         for (int i = 0; i < winningSizePerCard.size(); i++) {
             winners.put(i, 1);
