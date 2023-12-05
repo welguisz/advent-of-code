a32e2122c1aa36324b46c2cbaa1f50cbe2e03441
David Welguisz <welguisz@gmail.com>
2023-12-05 14:29:57 -0600

    Cleaned up the code.

--- src/com/dwelguisz/year2023/GiveASeedAFertilizer.java
+++ src/com/dwelguisz/year2023/GiveASeedAFertilizer.java
@@ -3,23 +3,21 @@ package com.dwelguisz.year2023;
 import com.dwelguisz.base.AoCDay;
 import com.dwelguisz.utilities.Coord3DLong;
 import org.apache.commons.lang3.tuple.Pair;
+import org.checkerframework.checker.units.qual.A;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 public class GiveASeedAFertilizer extends AoCDay {
 
     List<Long> seeds = new ArrayList<>();
-    List<Coord3DLong> seedToSoilMap = new ArrayList<>();
-    List<Coord3DLong> soilToFertilizerMap = new ArrayList<>();
-    List<Coord3DLong> fertilizerToWaterMap = new ArrayList<>();
-    List<Coord3DLong> waterToLightMap = new ArrayList<>();
-    List<Coord3DLong> lightToTemperatureMap = new ArrayList<>();
-    List<Coord3DLong> temperatureToHumidityMap = new ArrayList<>();
-    List<Coord3DLong> humidityToLocationMap = new ArrayList<>();
+    List<List<Coord3DLong>> transformers = new ArrayList<>();
 
     public void solve() {
         timeMarkers[0] = Instant.now().toEpochMilli();
@@ -33,45 +31,30 @@ public class GiveASeedAFertilizer extends AoCDay {
     }
 
     private void parseLines(List<String> lines) {
-        int tmp = 0;
+        List<Coord3DLong> transforms = new ArrayList<>();
         for (String line : lines) {
-            if (line.length() == 0) {
-                tmp++;
-            } else if (line.contains("map")) {
-
-            } else {
-                if (tmp == 0) {
-                    seeds = Arrays.stream(line.split(": ")[1].split(" ")).map(Long::parseLong).collect(Collectors.toList());
-                } else if (tmp == 1) {
-                    List<Long> nums = Arrays.stream(line.split(" ")).map(Long::parseLong).collect(Collectors.toList());
-                    seedToSoilMap.add(new Coord3DLong(nums.get(0), nums.get(1), nums.get(2)));
-                } else if (tmp == 2) {
-                    List<Long> nums = Arrays.stream(line.split(" ")).map(Long::parseLong).collect(Collectors.toList());
-                    soilToFertilizerMap.add(new Coord3DLong(nums.get(0), nums.get(1), nums.get(2)));
-                } else if (tmp == 3) {
-                    List<Long> nums = Arrays.stream(line.split(" ")).map(Long::parseLong).collect(Collectors.toList());
-                    fertilizerToWaterMap.add(new Coord3DLong(nums.get(0), nums.get(1), nums.get(2)));
-                } else if (tmp == 4) {
-                    List<Long> nums = Arrays.stream(line.split(" ")).map(Long::parseLong).collect(Collectors.toList());
-                    waterToLightMap.add(new Coord3DLong(nums.get(0), nums.get(1), nums.get(2)));
-                } else if (tmp == 5) {
-                    List<Long> nums = Arrays.stream(line.split(" ")).map(Long::parseLong).collect(Collectors.toList());
-                    lightToTemperatureMap.add(new Coord3DLong(nums.get(0), nums.get(1), nums.get(2)));
-                } else if (tmp == 6) {
-                    List<Long> nums = Arrays.stream(line.split(" ")).map(Long::parseLong).collect(Collectors.toList());
-                    temperatureToHumidityMap.add(new Coord3DLong(nums.get(0), nums.get(1), nums.get(2)));
-                } else if (tmp == 7) {
-                    List<Long> nums = Arrays.stream(line.split(" ")).map(Long::parseLong).collect(Collectors.toList());
-                    humidityToLocationMap.add(new Coord3DLong(nums.get(0), nums.get(1), nums.get(2)));
+            if (line.contains("map")) {
+                if (!transforms.isEmpty()) {
+                    transformers.add(new ArrayList<>(transforms));
+                    transforms = new ArrayList<>();
+                }
+            } else if (line.length() != 0) {
+                if (line.startsWith("seeds:")) {
+                    seeds = Arrays.stream(line.split(": ")[1].split("\\s+")).map(Long::parseLong).collect(Collectors.toList());
+                }
+                else {
+                    List<Long> nums = Arrays.stream(line.split("\\s+")).map(Long::parseLong).collect(Collectors.toList());
+                    transforms.add(new Coord3DLong(nums.get(0), nums.get(1), nums.get(2)));
                 }
             }
         }
+        transformers.add(transforms);
     }
 
     private Long processMap(List<Coord3DLong> map, Long val) {
         for(Coord3DLong m : map) {
             if (m.y <= val && (val < (m.y + m.z))) {
-                return m.x + (val - m.y);
+                return val + m.x - m.y;
             }
         }
         return val;
@@ -84,63 +67,43 @@ public class GiveASeedAFertilizer extends AoCDay {
     }
 
     private Long processAllMapsByOne(Long seed) {
-        final Long soil = processMap(seedToSoilMap, seed);
-        final Long fertilizer = processMap(soilToFertilizerMap, soil);
-        final Long water = processMap(fertilizerToWaterMap, fertilizer);
-        final Long light = processMap(waterToLightMap, water);
-        final Long temperature = processMap(lightToTemperatureMap, light);
-        final Long humidity = processMap(temperatureToHumidityMap, temperature);
-        return processMap(humidityToLocationMap, humidity);
+        Long value = seed;
+        for (List<Coord3DLong> transforms : transformers) {
+            value = processMap(transforms, value);
+        }
+        return value;
     }
 
     private Long processAllMapsByRange(Pair<Long,Long> seed) {
         Long seedEnd = seed.getLeft() + seed.getRight();
         //Sample here
         Long delta = (long) Math.sqrt(seed.getRight());
-        Long seedMin = Long.MAX_VALUE;
-        Long locationMin = Long.MAX_VALUE;
+        PriorityQueue<Pair<Long,Long>> pq = new PriorityQueue<>(20000, Comparator.comparingLong(Pair::getRight));
+        //Todo: Can this be turned into a LongStream
         for (Long s = seed.getLeft(); s < seedEnd; s += delta) {
             Long value = processAllMapsByOne(s);
-            if (value < locationMin) {
-                locationMin = value;
-                seedMin = s;
-            }
+            pq.add(Pair.of(s,value));
         }
-        Long value = processAllMapsByOne(seedEnd);
-        if (value < locationMin) {
-            locationMin = value;
-            seedMin = seedEnd;
-        }
-        System.out.println(String.format("seedMin: %d; locationMin: %d", seedMin, locationMin));
-        Long seedSubStart = (seedMin - delta) < seed.getLeft() ? seed.getLeft() : seedMin - delta;
-        Long seedSubEnd = (seedMin + delta) > seedEnd ? seedEnd : seedMin + delta;
-        for (Long s = seedSubStart; s < seedSubEnd; s+= 1) {
-            value = processAllMapsByOne(s);
-            if (value < locationMin) {
-                locationMin = value;
-            }
-        }
-        System.out.println(String.format("locationMin: %d", locationMin));
-        return locationMin;
+        Pair<Long,Long> mins = pq.poll();
+        Long seedSubStart = (mins.getLeft() - delta) < seed.getLeft() ? seed.getLeft() : mins.getLeft() - delta;
+        Long seedSubEnd = (mins.getLeft() + delta) > seedEnd ? seedEnd : mins.getLeft() + delta;
+        return LongStream.range(seedSubStart, seedSubEnd)
+                .map(l -> processAllMapsByOne(l))
+                .min().getAsLong();
     }
 
 
     private Long solutionPart2() {
         List<Pair<Long, Long>> newSeeds = new ArrayList<>();
         for (int i = 0; i < seeds.size(); i+=2) {
-            newSeeds.add(Pair.of(seeds.get(i), seeds.get(i) + seeds.get(i+1)));
+            newSeeds.add(Pair.of(seeds.get(i), seeds.get(i+1)));
         }
         List<Long> mins = new ArrayList<>();
         for (Pair<Long, Long> s : newSeeds) {
             mins.add(processAllMapsByRange(s));
         }
-        List<Long> ordered = mins.stream()
-                .filter(v -> v > 0)
-                .sorted()
-                .collect(Collectors.toList());
-        System.out.println("Min list: " + ordered.stream()
-                        .map(l -> l.toString())
-                .collect(Collectors.joining(", ")));
-        return ordered.get(0);
+        return mins.stream()
+                .min(Long::compare)
+                .get();
     }
 }
