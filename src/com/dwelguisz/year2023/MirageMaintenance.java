d1713e185ae511383246f9bc5fd3a4eae8d76b8c
David Welguisz <welguisz@gmail.com>
2023-12-08 23:52:30 -0600

    Converted to streams. Now on to using a functional interface.

--- src/com/dwelguisz/year2023/MirageMaintenance.java
+++ src/com/dwelguisz/year2023/MirageMaintenance.java
@@ -5,6 +5,7 @@ import com.dwelguisz.base.AoCDay;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 import java.util.stream.Collectors;
@@ -14,43 +15,40 @@ public class MirageMaintenance extends AoCDay {
     public void solve() {
         timeMarkers[0] = Instant.now().toEpochMilli();
         List<String> lines = readResoruceFile(2023, 9, false, 0);
-        List<List<Long>> ints = lines.stream().map(
+        List<List<Long>> values = lines.stream().map(
                 l -> Arrays.stream(l.split(" "))
                         .map(Long::parseLong)
                         .collect(Collectors.toList())
         ).collect(Collectors.toList());;
         timeMarkers[1] = Instant.now().toEpochMilli();
-        part1Answer = solutionPart1(ints, false);
+        part1Answer = solutionPart1(values);
         timeMarkers[2] = Instant.now().toEpochMilli();
-        part2Answer = solutionPart1(ints, true);
+        part2Answer = solutionPart2(values);
         timeMarkers[3] = Instant.now().toEpochMilli();
     }
 
-    Stack<List<Long>> createStack(List<Long> values) {
-        Stack<List<Long>> stack = new Stack<>();
-        stack.push(values);
+    List<List<Long>> createStack(List<Long> values) {
+        List<List<Long>> stack = new ArrayList<>();
+        stack.add(values);
         List<Long> currentRow = new ArrayList<>(values);
         while (!currentRow.stream().allMatch(i -> i == 0l)) {
             final List<Long> t = currentRow;
             List<Long> diffRow = IntStream.range(0, currentRow.size() - 1).boxed()
                     .map(i -> t.get(i + 1) - t.get(i))
                     .collect(Collectors.toList());
-            stack.push(diffRow);
+            stack.add(diffRow);
             currentRow = new ArrayList<>(diffRow);
         }
+        Collections.reverse(stack);
         return stack;
     }
-    Long solutionPart1(List<List<Long>> ints, boolean part2) {
-        Long total = 0L;
-        for (List<Long> intList : ints) {
-            Stack<List<Long>> stack = createStack(intList);
-            Long sum = 0L;
-            while (!stack.isEmpty()) {
-                List<Long> cr = stack.pop();
-                sum = part2 ? cr.get(0) - sum : sum + cr.get(cr.size() - 1);
-            }
-            total += sum;
-        }
-        return total;
+
+    Long solutionPart1(List<List<Long>> ints) {
+        return ints.stream().map(r -> createStack(r).stream().map(row -> row.get(row.size()-1)).reduce(0L, (a,b) -> a+b)).reduce(0L,(a,b) -> a+b);
     }
+
+    Long solutionPart2(List<List<Long>> ints) {
+        return ints.stream().map(r -> createStack(r).stream().map(row -> row.get(0)).reduce(0L, (a,b) -> b-a)).reduce(0L,(a,b) -> a+b);
+    }
+
 }
