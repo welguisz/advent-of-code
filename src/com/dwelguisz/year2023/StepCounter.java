c461d58a4a2a2dfe85621bca42bae8828559b011
David Welguisz <welguisz@gmail.com>
2023-12-21 20:39:42 -0600

    Fixed my small bug. Now computes and runs in an under 1 second

--- src/com/dwelguisz/year2023/StepCounter.java
+++ src/com/dwelguisz/year2023/StepCounter.java
@@ -53,13 +53,10 @@ public class StepCounter extends AoCDay {
     }
 
     Long solutionPart1(Character[][] grid, Coord2D startingPosition) {
-        Set<String> positions = new HashSet<>();
         Set<Coord2D> states = new HashSet<>();
         states.add(startingPosition);
-        positions.add(startingPosition.toString());
         for (int i = 0; i < 64; i++) {
             states = states.stream().flatMap(s -> nextSpots(s,grid)).collect(Collectors.toSet());
-            positions.addAll(states.stream().map(s -> s.toString()).collect(Collectors.toList()));
         }
         return (long) states.size();
     }
@@ -72,76 +69,57 @@ public class StepCounter extends AoCDay {
     }
 
     Integer newX(Integer x, Integer length) {
-        return ((x % length) + length) % length;
+        return (x + length) % length;
     }
-    Double solutionPart2(Character[][] grid, Coord2D startingPosition) {
+    Long solutionPart2(Character[][] grid, Coord2D startingPosition) {
         Set<Coord2D> states = new HashSet<>();
         states.add(startingPosition);
         int stepCount;
-        Map<Coord2D, Integer> distances = new HashMap<>();
-        distances.put(startingPosition, 0);
+        int oddOrEven = 1;
         List<Long> oddTotals = new ArrayList<>();
-        Long totalReached = 0L;
+        Map<Boolean, Set<Coord2D>> lastSetOfCoords = new HashMap<>();
+        lastSetOfCoords.put(false, new HashSet<>());
+        lastSetOfCoords.put(true, new HashSet<>());
+        Map<Boolean, Long> runningSum = new HashMap<>();
+        runningSum.put(false,0L);
+        runningSum.put(true,0L);
         for (stepCount = 0; true; stepCount++) {
-            states = states.stream().flatMap(s -> nextSpotsPart2(s, grid)).collect(Collectors.toSet());
-            if (stepCount % 2 == 1) {
-                totalReached += states.size();
-                if (stepCount % 131 == 65) {
-                    oddTotals.add(totalReached);
-                }
-                //We need at least 4 points to do a quadratic. 5 will guarantee a zero in createStack
-                if (oddTotals.size() == 3) {
-                    break;
-                }
+            final Integer finalStepCount = stepCount;
+            states = states.stream().flatMap(s -> nextSpotsPart2(s, grid))
+                    .filter(s -> !lastSetOfCoords.get(finalStepCount%2==oddOrEven).contains(s))
+                    .collect(Collectors.toSet());
+            Set<Coord2D> finalStates = states;
+            runningSum.computeIfPresent(stepCount%2==oddOrEven,(k, v) -> v + finalStates.size());
+            lastSetOfCoords.put(stepCount%2==oddOrEven,states);
+            if (stepCount % 262 == 65) {
+                oddTotals.add(runningSum.get(stepCount%2==(oddOrEven^1)));
+            }
+            //We need at least 4 points to do a quadratic. 5 will guarantee a zero in createStack
+            if (oddTotals.size() == 5) {
+                break;
             }
         }
-        // We are solving for coefficients in a quadratic
-        // f(1) = a + b + c = oddTotals.get(0)
-        // f(2) = 4a + 2b + c = oddTotals.get(1)
-        // f(3) = 9a + 3b + c = oddTotals.get(2)
-        // -4*f(1)+f(2) -> f(2)  = -2b -3c = y-4x
-        // -9*f(1)+f(3) -> f(3) = -6b -8c = z - 9x
-        // -0.5*f(2) -> f(2) = b + 3/2c = = -0.5(y-4x)
-        // -6*f(2) + f(3) -> f(3) = c = z-9x+
-        // -1.5*f(3) + f(2) -> f(2)
-        // -1*f(3) + f(1) -> f(1)
-        // -1*f(2) + f(2) -> f(2)
-        Double a = Double.valueOf(oddTotals.get(0));
-        Double b = Double.valueOf(oddTotals.get(1));
-        Double c = Double.valueOf(oddTotals.get(2));
-        b += -4*a;
-        c += -9*a;
-        b *= -0.5;
-        c += -6*b;
-        b += -1.5*c;
-        a += -1 * c;
-        a += -1 * b;
-        Long loopCount = 26501365L / 131;
-        return a*loopCount*loopCount+b*loopCount+c;
-
-        //Belief that this is a quadratic, let's do
-//        Long loopCount = 26501365L / 262 - 1;
-//        Long currentLoopCount = i / 262L - 1;
-//        Long deltaLoopCount = loopCount - currentLoopCount;
-//        long deltaLoopCountTriangular = (loopCount * (loopCount+1))/2 - (currentLoopCount * (currentLoopCount+1))/2;
-//        Long deltaDelta = deltaDeltas.get(deltaDeltas.size()-1);
-//        Long initialDelta = deltas.get(0);
-//        return (deltaDelta * deltaLoopCountTriangular + initialDelta * deltaLoopCount + totalReached);
+        List<Long> oddCoefficients = findCoefficients(oddTotals.get(0), oddTotals.get(1), oddTotals.get(2));
+        Long loopCount = 26501365L/ 262;
+        Long jumpValue = function(oddCoefficients.get(0),oddCoefficients.get(1),oddCoefficients.get(2), loopCount+1);
+        return jumpValue;
     }
 
-    List<List<Long>> createStack(List<Long> values) {
-        List<List<Long>> stack = new ArrayList<>();
-        stack.add(values);
-        List<Long> currentRow = new ArrayList<>(values);
-        for(int ix = 0; ix < 2; ix++) {
-            final List<Long> t = currentRow;
-            List<Long> diffRow = IntStream.range(0, currentRow.size() - 1).boxed()
-                    .map(i -> t.get(i + 1) - t.get(i))
-                    .collect(Collectors.toList());
-            stack.add(diffRow);
-            currentRow = new ArrayList<>(diffRow);
-        }
-        Collections.reverse(stack);
-        return stack;
+    List<Long> findCoefficients(Long x, Long y, Long z) {
+        Double aDouble = Double.valueOf(x);
+        Double bDouble = Double.valueOf(y);
+        Double cDouble = Double.valueOf(z);
+        bDouble += -4*aDouble;
+        cDouble += -9*aDouble;
+        bDouble *= -0.5;
+        cDouble += 6*bDouble;
+        bDouble += -1.5*cDouble;
+        aDouble += -1 * cDouble;
+        aDouble += -1 * bDouble;
+        return List.of(aDouble.longValue(), bDouble.longValue(), cDouble.longValue());
+    }
+
+    Long function(Long a, Long b, Long c, Long x) {
+        return a*x*x+b*x+c;
     }
 }
