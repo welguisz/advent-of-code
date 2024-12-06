34d6de8741831575cf7bf1c7f28a797ba48d89c7
David Welguisz <welguisz@gmail.com>
2024-12-06 17:15:24 -0600

    Cleaned up Day 6 logic.

---
+++ src/com/dwelguisz/utilities/Grid.java
@@ -0,0 +1,45 @@
+package com.dwelguisz.utilities;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Grid {
+
+    public static List<Coord2D> DIRECTIONS = List.of(
+            new Coord2D(-1,-0),
+            new Coord2D(0, 1),
+            new Coord2D(1, 0),
+            new Coord2D(0, -1))
+            ;
+
+
+    public static char[][] createCharGrid(List<String> lines) {
+        char[][] grid = new char[lines.size()][lines.get(0).length()];
+        int i = 0;
+        for(String line : lines) {
+            for (int j = 0; j < line.length(); j++) {
+                grid[i][j] = line.charAt(j);
+            }
+            i++;
+        }
+        return grid;
+    }
+
+    public static Map<Coord2D, Character> createCharGridMap(List<String> lines) {
+        Map<Coord2D, Character> grid = new HashMap<>();
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = 0; j < lines.get(i).length(); j++) {
+                grid.put(new Coord2D(i, j), lines.get(i).charAt(j));
+            }
+        }
+        return grid;
+    }
+
+    public static Coord2D getStartingPoint(Map<Coord2D, Character> grid, char startingSymbol) {
+        return grid.entrySet().stream()
+                .filter(entry -> entry.getValue() == startingSymbol)
+                .findFirst().get().getKey();
+    }
+
+}
--- src/com/dwelguisz/year2024/GuardGallivant.java
+++ src/com/dwelguisz/year2024/GuardGallivant.java
@@ -7,13 +7,16 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.Instant;
 import java.util.*;
 
+import static com.dwelguisz.utilities.Grid.createCharGridMap;
+import static com.dwelguisz.utilities.Grid.getStartingPoint;
+import static com.dwelguisz.utilities.Grid.DIRECTIONS;
+
 public class GuardGallivant extends AoCDay {
     public void solve() {
         timeMarkers[0] = Instant.now().toEpochMilli();
         List<String> lines = readResoruceFile(2024, 6, false, 0);
-        char[][] grid = convertToCharGrid(lines);
-        Map<Coord2D, Character> gridHash = createGridHash(grid);
-        Coord2D startingPoint = gridHash.entrySet().stream().filter(entry -> entry.getValue() == '^').findFirst().get().getKey();
+        Map<Coord2D, Character> gridHash = createCharGridMap(lines);
+        Coord2D startingPoint = getStartingPoint(gridHash, '^');
         timeMarkers[1] = Instant.now().toEpochMilli();
         Set<Coord2D> visited = solutionPart1(gridHash, startingPoint);
         part1Answer = visited.size();
@@ -22,40 +25,34 @@ public class GuardGallivant extends AoCDay {
         timeMarkers[3] = Instant.now().toEpochMilli();
     }
 
-    List<Coord2D> DIRECTIONS = List.of(
-            new Coord2D(-1,-0),
-            new Coord2D(0, 1),
-            new Coord2D(1, 0),
-            new Coord2D(0, -1))
-            ;
-
-    Map<Coord2D, Character> createGridHash(char[][] grid) {
-        Map<Coord2D, Character> gridHash = new HashMap<>();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                gridHash.put(new Coord2D(i, j), grid[i][j]);
-            }
-        }
-        return gridHash;
-    }
-
-    Set<Coord2D> solutionPart1(Map<Coord2D, Character> gridHash, Coord2D start) {
+    Set<Coord2D> guardPath(Map<Coord2D, Character> grid, Coord2D startingPoint) {
         Set<Coord2D> visited = new HashSet<>();
         int dirIndex = 0;
-        Coord2D curr = start;
-        while (true) {
+        Coord2D curr = startingPoint;
+        Set<Pair<Coord2D, Coord2D>> memory = new HashSet<>();
+        boolean loop = true;
+        while (!memory.contains(Pair.of(curr, DIRECTIONS.get(dirIndex)))) {
+            memory.add(Pair.of(curr, DIRECTIONS.get(dirIndex)));
             visited.add(curr);
             Coord2D next = curr.add(DIRECTIONS.get(dirIndex));
-            if (!gridHash.containsKey(next)) {
+            if (memory.contains(Pair.of(next, DIRECTIONS.get(dirIndex)))) {
+                break;
+            }
+            if (!grid.containsKey(next)) {
                 return visited;
             } else {
-                if (gridHash.get(next) == '#') {
+                if (grid.get(next) == '#') {
                     dirIndex = (dirIndex + 1) % 4;
                 } else {
                     curr = next;
                 }
             }
         }
+        return new HashSet<>();
+    }
+
+    Set<Coord2D> solutionPart1(Map<Coord2D, Character> gridHash, Coord2D start) {
+        return guardPath(gridHash, start);
     }
 
     long solutionPart2(Map<Coord2D, Character> gridHash, Coord2D start, Set<Coord2D> visited) {
@@ -65,24 +62,8 @@ public class GuardGallivant extends AoCDay {
                 continue;
             }
             gridHash.put(obstacle, '#');
-            Coord2D curr = start;
-            int dirIndex = 0;
-            boolean loop = true;
-            Set<Pair<Coord2D, Coord2D>> memory = new HashSet<>();
-            while (!memory.contains(Pair.of(curr, DIRECTIONS.get(dirIndex)))) {
-                memory.add(Pair.of(curr, DIRECTIONS.get(dirIndex)));
-                Coord2D next = curr.add(DIRECTIONS.get(dirIndex));
-                if ( !gridHash.containsKey(next)) {
-                    loop = false;
-                    break;
-                }
-                if (gridHash.get(next) == '#') {
-                    dirIndex = (dirIndex + 1) % 4;
-                } else {
-                    curr = next;
-                }
-            }
-            if (loop) {
+            Set<Coord2D> path = guardPath(gridHash, start);
+            if (path.isEmpty()) {
                 createLoop++;
             }
             gridHash.put(obstacle, '.');
