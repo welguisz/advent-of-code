8ac59279a8672e3c5b17b36bb0bab870333a2ff2
David Welguisz <welguisz@gmail.com>
2024-12-07 12:06:18 -0600

    Did a solution using % / -. Took time from 350 ms to 7 ms.

--- src/com/dwelguisz/year2024/BridgeRepair.java
+++ src/com/dwelguisz/year2024/BridgeRepair.java
@@ -32,18 +32,18 @@ public class BridgeRepair extends AoCDay {
 
     long solutionPart1(List<Pair<Long, List<Long>>> equations) {
         return equations.stream()
-                .filter(e -> validEquationPart1(e.getLeft(), e.getRight()))
+                .filter(e -> validEquationPart1_R2l(e.getLeft(), e.getRight(), false))
                 .map(Pair::getLeft)
                 .mapToLong(l -> l).sum();
     }
 
-    boolean validEquationPart1(Long total, List<Long> operands) {
-        List<Long> possibleAnswers = new ArrayList<>();
+    boolean validEquationPart1_l2R(Long total, List<Long> operands, boolean part2) {
+        Queue<Long> possibleAnswers = new LinkedList<>();
         possibleAnswers.add(operands.get(0));
         for (int i = 1; i < operands.size(); i++) {
             int currentSize = possibleAnswers.size();
             while (currentSize > 0) {
-                long temp = possibleAnswers.remove(0);
+                long temp = possibleAnswers.poll();
                 long mulTotal = temp * operands.get(i);
                 long addTotal = temp + operands.get(i);
                 if (mulTotal <= total) {
@@ -52,43 +52,57 @@ public class BridgeRepair extends AoCDay {
                 if (addTotal <= total) {
                     possibleAnswers.add(addTotal);
                 }
+                if (part2) {
+                    Long concatTotal = Long.parseLong(temp + operands.get(i).toString());;
+                    if (concatTotal <= total) {
+                        possibleAnswers.add(concatTotal);
+                    }
+                }
                 currentSize--;
             }
         }
         return possibleAnswers.contains(total);
     }
+
+    boolean validEquationPart1_R2l(Long total, List<Long> operands, boolean part2) {
+        Queue<Long> possibleAnswers = new LinkedList<>();
+        Stack<Long> stackOperands = new Stack<>();
+        operands.forEach(operand -> stackOperands.push(operand));
+        possibleAnswers.add(total);
+        while (stackOperands.size() > 1 && !possibleAnswers.isEmpty()) {
+            int currentSize = possibleAnswers.size();
+            long operand = stackOperands.pop();
+            while (currentSize > 0) {
+                long temp = possibleAnswers.poll();
+                if (temp - operand >= 0) {
+                    possibleAnswers.add(temp - operand);
+                }
+                if (temp % operand == 0) {
+                    possibleAnswers.add(temp / operand);
+                }
+                if (part2) {
+                    int operandLength = String.valueOf(operand).length();
+                    String tempStr = String.valueOf(temp);
+                    if (tempStr.length() >= operandLength) {
+                        String firstPart = tempStr.substring(0, tempStr.length() - operandLength);
+                        String secondPart = tempStr.substring(tempStr.length() - operandLength);
+                        if (secondPart.equals(String.valueOf(operand)) && !firstPart.isEmpty()) {
+                            possibleAnswers.add(Long.parseLong(firstPart));
+                        }
+                    }
+                }
+                currentSize--;
+            }
+        }
+        return possibleAnswers.contains(stackOperands.pop());
+    }
+
 
     long solutionPart2(List<Pair<Long, List<Long>>> equations) {
         return equations.stream()
-                .filter(e -> !validEquationPart1(e.getLeft(), e.getRight()))
-                .filter(e -> validEquationPart2(e.getLeft(), e.getRight()))
+                .filter(e -> !validEquationPart1_R2l(e.getLeft(), e.getRight(), false))
+                .filter(e -> validEquationPart1_R2l(e.getLeft(), e.getRight(), true))
                 .map(Pair::getLeft)
                 .mapToLong(l -> l).sum();
     }
-
-    boolean validEquationPart2(Long total, List<Long> operands) {
-        Queue<Long> possibleAnswers = new LinkedList<>();
-        possibleAnswers.add(operands.get(0));
-        for (int i = 1; i < operands.size(); i++) {
-            int currentSize = possibleAnswers.size();
-            while (currentSize > 0) {
-                Long temp = possibleAnswers.poll();
-                Long mulTotal = temp * operands.get(i);
-                Long addTotal = temp + operands.get(i);
-                Long concatTotal = Long.parseLong(temp + operands.get(i).toString());;
-                if (mulTotal <= total) {
-                    possibleAnswers.add(mulTotal);
-                }
-                if (addTotal <= total) {
-                    possibleAnswers.add(addTotal);
-                }
-                if (concatTotal <= total) {
-                    possibleAnswers.add(concatTotal);
-                }
-                currentSize--;
-            }
-        }
-        return possibleAnswers.contains(total);
-    }
-
 }
