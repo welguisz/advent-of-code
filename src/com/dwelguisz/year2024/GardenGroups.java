227efe767fc67638b2ef1cb2571fa934d3fad7c6
David Welguisz <welguisz@gmail.com>
2024-12-12 17:30:35 -0600

    A little refactor.

--- src/com/dwelguisz/year2024/GardenGroups.java
+++ src/com/dwelguisz/year2024/GardenGroups.java
@@ -5,6 +5,7 @@ import com.dwelguisz.utilities.Coord2D;
 
 import java.time.Instant;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -21,75 +22,93 @@ public class GardenGroups extends AoCDay {
         timeMarkers[0] = Instant.now().toEpochMilli();
         List<String> lines = readResoruceFile(2024, 12, false, 0);
         Map<Coord2D, Character> plots = createCharGridMap(lines);
-        int maxY = lines.get(0).length();
-        int maxX = lines.size();
+        Map<Character, List<Coord2D>> gardenGroups = createGardenGroup(lines);
         timeMarkers[1] = Instant.now().toEpochMilli();
-        part1Answer = bothParts(plots, maxX, maxY, false);
+        part1Answer = calculatePrice(gardenGroups, false);
         timeMarkers[2] = Instant.now().toEpochMilli();
-        part2Answer = bothParts(plots, maxX, maxY, true);
+        part2Answer = calculatePrice(gardenGroups, true);
         timeMarkers[3] = Instant.now().toEpochMilli();
     }
 
-    long bothParts(Map<Coord2D, Character> plots, int maxX, int maxY, boolean part2) {
+    Map<Character, List<Coord2D>> createGardenGroup(List<String> lines) {
+        Map<Character, List<Coord2D>> gardenGroups = new HashMap<>();
+        int x = 0;
+        for (String line : lines) {
+            for (int y = 0; y < line.length(); y++) {
+                gardenGroups.computeIfAbsent(line.charAt(y), c -> new ArrayList<>()).add(new Coord2D(x, y));
+            }
+            x++;
+        }
+        return gardenGroups;
+    }
+
+    long calculatePrice(Map<Character, List<Coord2D>> groups, boolean part2) {
+        return groups.entrySet().stream().map(e -> calculatePricePerGroup(e.getKey(), e.getValue(), part2))
+                .mapToLong(l -> l).sum();
+    }
+
+    long calculatePricePerGroup(Character c, List<Coord2D> group, boolean part2) {
+        long total = 0L;
         Set<Coord2D> seen = new HashSet<>();
-        long price = 0;
-        for (int x = 0; x < maxX; x++) {
-            for (int y = 0; y < maxY; y++) {
-                if (seen.contains(new Coord2D(x, y))) {
+        for (Coord2D currentLoc : group) {
+            if (seen.contains(currentLoc)) {
+                continue;
+            }
+            ArrayDeque<Coord2D> queue = new ArrayDeque<>();
+            queue.add(currentLoc);
+            long perimeter = 0;
+            long area = 0;
+            Map<Coord2D, Set<Coord2D>> perimeters = new HashMap<>();
+            while (!queue.isEmpty()) {
+                Coord2D current = queue.pollFirst();
+                if (seen.contains(current)) {
                     continue;
                 }
+                seen.add(current);
+                area++;
+                for (Coord2D dir : NEIGHBORS) {
+                    Coord2D next = current.add(dir);
+                    if (group.contains(next)) {
+                        queue.add(next);
+                    } else {
+                        perimeter++;
+                        perimeters.computeIfAbsent(dir, k -> new HashSet<>()).add(current);
+                    }
+                }
+            }
+            long sides = findSides(perimeters);
+            long multiplier = part2 ? sides : perimeter;
+            total += area * multiplier;
+        }
+        return total;
+    }
+
+    long findSides(Map<Coord2D, Set<Coord2D>> perimeter) {
+        long sides = 0;
+        for (Map.Entry<Coord2D, Set<Coord2D>> entry : perimeter.entrySet()) {
+            Set<Coord2D> seen = new HashSet<>();
+            for (Coord2D current : entry.getValue()) {
+                if (seen.contains(current)) {
+                    continue;
+                }
+                sides++;
                 ArrayDeque<Coord2D> queue = new ArrayDeque<>();
-                queue.add(new Coord2D(x, y));
-                long area = 0;
-                long perimeter = 0;
-                Map<Coord2D, Set<Coord2D>> perimeters = new HashMap<>();
+                queue.add(current);
                 while (!queue.isEmpty()) {
-                    Coord2D curr = queue.pollFirst();
-                    if (seen.contains(curr)) {
+                    Coord2D test = queue.pollFirst();
+                    if (seen.contains(test)) {
                         continue;
                     }
-                    seen.add(curr);
-                    area++;
+                    seen.add(test);
                     for (Coord2D dir : NEIGHBORS) {
-                        Coord2D next = curr.add(dir);
-                        if (plots.containsKey(next) && (plots.get(curr) == plots.get(next))) {
+                        Coord2D next = test.add(dir);
+                        if (entry.getValue().contains(next)) {
                             queue.add(next);
-                        } else {
-                            perimeter++;
-                            perimeters.computeIfAbsent(dir, k -> new HashSet<>()).add(curr);
                         }
                     }
                 }
-                long sides = 0;
-                if (part2) {
-                    for (Map.Entry<Coord2D, Set<Coord2D>> entry : perimeters.entrySet()) {
-                        Set<Coord2D> seenPerimeters = new HashSet<>();
-                        for (Coord2D curr : entry.getValue()) {
-                            if (seenPerimeters.contains(curr)) {
-                                continue;
-                            }
-                            sides++;
-                            ArrayDeque<Coord2D> perimQueue = new ArrayDeque<>();
-                            perimQueue.add(curr);
-                            while (!perimQueue.isEmpty()) {
-                                Coord2D test = perimQueue.pollFirst();
-                                if (seenPerimeters.contains(test)) {
-                                    continue;
-                                }
-                                seenPerimeters.add(test);
-                                for (Coord2D dir : NEIGHBORS) {
-                                    Coord2D next = test.add(dir);
-                                    if (entry.getValue().contains(next)) {
-                                        perimQueue.add(next);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                price += part2 ? area * sides : area * perimeter;
             }
         }
-        return price;
+        return sides;
     }
 }
