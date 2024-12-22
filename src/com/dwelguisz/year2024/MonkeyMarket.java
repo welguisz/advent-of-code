267104bb2dbf1fbd8e5e050ca425a747aa04e211
David Welguisz <welguisz@gmail.com>
2024-12-22 11:56:10 -0600

    Change from Map.computeIfAbsent/Map.compute to Map.merge. Changed from multiplication/division to left shifting, right shifting AND using bitwise-AND.

--- src/com/dwelguisz/year2024/MonkeyMarket.java
+++ src/com/dwelguisz/year2024/MonkeyMarket.java
@@ -16,7 +16,7 @@ public class MonkeyMarket extends AoCDay {
     public void solve() {
         timeMarkers[0] = Instant.now().toEpochMilli();
         List<String> lines = readResoruceFile(2024, 22, false, 0);
-        List<Long> startingSecrets = lines.stream().map(Long::parseLong).toList();
+        List<Integer> startingSecrets = lines.stream().map(Integer::parseInt).toList();
         timeMarkers[1] = Instant.now().toEpochMilli();
         part1Answer = solutionPart1(startingSecrets);
         timeMarkers[2] = Instant.now().toEpochMilli();
@@ -24,45 +24,44 @@ public class MonkeyMarket extends AoCDay {
         timeMarkers[3] = Instant.now().toEpochMilli();
     }
 
-    long calculateOneSecret(long startingSecret) {
-        long x = startingSecret;
+    int calculateOneSecret(int startingSecret) {
+        int x = startingSecret;
         for (int i = 0; i < 2000; i++) {
             x = oneEvolution(x);
         }
         return x;
     }
 
-    long solutionPart1(List<Long> startingSecrets) {
-        return startingSecrets.stream().map(this::calculateOneSecret).reduce(0L, Long::sum);
+    long solutionPart1(List<Integer> startingSecrets) {
+        return startingSecrets.stream().mapToLong(this::calculateOneSecret).reduce(0L, Long::sum);
     }
 
-    long oneEvolution(long secret) {
-        long x = secret;
-        x = ((x * 64) ^ x) % 16777216;
-        x = ((x / 32) ^ x) % 16777216;
-        return ((x * 2048) ^ x) % 16777216;
+    int oneEvolution(int secret) {
+        int x = secret;
+        x = ((x << 6) ^ x) & 0xFFFFFF;
+        x = ((x >> 5) ^ x) & 0xFFFFFF;
+        return ((x << 11) ^ x) & 0xFFFFFF;
     }
 
-    long solutionPart2(List<Long> startingSecrets) {
+    long solutionPart2(List<Integer> startingSecrets) {
         Map<List<Integer>, Integer> total = new HashMap<>();
-        for (Long secret : startingSecrets) {
+        for (Integer secret : startingSecrets) {
             List<Coord2D> pattern = new ArrayList<>();
-            Long last = secret % 10;
-            long x = secret;
+            int last = secret % 10;
+            int x = secret;
             for (int i = 0; i < 2000; i++) {
                 x = oneEvolution(x);
-                long temp = x % 10;
-                pattern.add(new Coord2D((int) (temp - last), (int) temp));
+                int temp = x % 10;
+                pattern.add(new Coord2D(temp - last, temp));
                 last = temp;
             }
             Set<List<Integer>> seen = new HashSet<>();
             for (int i = 0; i < pattern.size()-4; i++) {
                 List<Integer> pat = pattern.subList(i, i+4).stream().map(m -> m.x).toList();
-                Integer value = pattern.get(i+3).y;
                 if (!seen.contains(pat)) {
+                    Integer value = pattern.get(i+3).y;
                     seen.add(pat);
-                    total.computeIfAbsent(pat, k -> 0);
-                    total.compute(pat, (k, v) -> v + value);
+                    total.merge(pat, value, Integer::sum);
                 }
             }
         }
