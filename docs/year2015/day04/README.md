a0a762b088d15e181171a870bdcb20b0932ddd19
David Welguisz <welguisz@gmail.com>
2025-02-25 01:01:59 -0600

    Did documentation for Year 2015, Day 4.

--- docs/year2015/day04/README.md
+++ docs/year2015/day04/README.md
@@ -1,14 +1,16 @@
-# Day 4: The Ideal Stocking Stuffer
+# Day 4 The Ideal Stocking Stuffer
 
 [Back to Top README file](../../../README.md)
+
 ## Overview
-Difficult Level: Easy
 
-Input: 8 random letters
+* [Puzzle Prompt](https://adventofcode.com/2015/day/4)
+* Difficult Level: 2 out of 10
+* [Input](https://adventofcode.com/2015/day/4/input): 8 random letter
+* Skills/Knowledge: MD5 Encryption
 
-Find the lowest integer that produces a MD5 hash that has a certain characterization
+## Part 1 Solution:
 
-## Part 1
 * Start a counter at 0.
 * Create a string that contains your input string plus a counter.
 * Do a MD5 hash on the created String
@@ -34,8 +36,12 @@ Find the lowest integer that produces a MD5 hash that has a certain characteriza
     }
 ```
 
-## Part 2
-Same as Part 1, but the first 6 characters of the digest are zero.
+
+## Part 2 Solution:
+
+Same as Part 1, but the first 6 characters of the digest are zero. If you want, you can start from your answer in Part1 since
+for Part2 to be true, the first 5 characters have to be zero, so you can skip all the integers from
+0 to `Part1 Answer`.
 
 ```java
     public Integer solutionPart2(String input) {
@@ -57,4 +63,17 @@ Same as Part 1, but the first 6 characters of the digest are zero.
     }
 ```
 
-|[Previous (Day 3)](../day03/README.md)|[Next (Day 5)](../day05/README.md)|
+## Times
+
+* Parsing: 4 ms
+* Part 1 Solve time: 130 ms
+* Part 2 Solve time: 1803 ms
+
+## Solutions: 
+
+* Part 1: 346386
+* Part 2: 9958218
+
+| | |
+|:---|---:|
+|[Previous (Year 2015, Day 3)](../../year2015/day03/README.md)|[Next (Year 2015, Day 5)](../../year2015/day05/README.md)|
--- src/com/dwelguisz/year2015/TheIdealStockingStuffer.java
+++ src/com/dwelguisz/year2015/TheIdealStockingStuffer.java
@@ -15,8 +15,10 @@ public class TheIdealStockingStuffer extends AoCDay {
         timeMarkers[1] = Instant.now().toEpochMilli();
         part1Answer = solutionPart1(instructions.get(0));
         timeMarkers[2] = Instant.now().toEpochMilli();
-        part2Answer = solutionPart2(instructions.get(0));
+        part2Answer = solutionPart2(instructions.get(0), Integer.parseInt(part1Answer.toString()));
         timeMarkers[3] = Instant.now().toEpochMilli();
+        printExplanation = true;
+        documentation();
     }
 
     public Integer solutionPart1(String input) {
@@ -36,6 +38,61 @@ public class TheIdealStockingStuffer extends AoCDay {
             i++;
         }
     }
+    public int solutionPart2(String input, int part1Answer) {
+        int i = part1Answer;
+        while (true) {
+            String str = input.concat(""+i);
+            try {
+                byte[] bytesOfMessage = str.getBytes(StandardCharsets.UTF_8);
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                byte[] theMD5digest = md.digest(bytesOfMessage);
+                if ((theMD5digest[0] == 0) && (theMD5digest[1] == 0) && (theMD5digest[2] == 0)) {
+                    return i;
+                }
+            } catch (NoSuchAlgorithmException e) {
+                System.out.println("Bad algorithm selected");
+            }
+            i++;
+        }
+    }
+
+    void documentation() {
+        puzzleName = "The Ideal Stocking Stuffer";
+        difficultLevel = "2 out of 10";
+        inputDescription = "8 random letter";
+        skills = List.of("MD5 Encryption");
+        part1Solution = """
+* Start a counter at 0.
+* Create a string that contains your input string plus a counter.
+* Do a MD5 hash on the created String
+* See if the string starts with 5 zeroes.
+
+```java
+    public Integer solutionPart1(String input) {
+        Integer i = 0;
+        while (true) {
+            String str = input.concat(i.toString());
+            try {
+                byte[] bytesOfMessage = str.getBytes(StandardCharsets.UTF_8);
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                byte[] theMD5digest = md.digest(bytesOfMessage);
+                if ((theMD5digest[0] == 0) && (theMD5digest[1] == 0) && (theMD5digest[2] >= 0) && (theMD5digest[2] < 16)) {
+                    return i;
+                }
+            } catch (NoSuchAlgorithmException e) {
+                System.out.println("Bad algorithm selected");
+            }
+            i++;
+        }
+    }
+```
+""";
+        part2Solution = """
+Same as Part 1, but the first 6 characters of the digest are zero. If you want, you can start from your answer in Part1 since
+for Part2 to be true, the first 5 characters have to be zero, so you can skip all the integers from
+0 to `Part1 Answer`.
+
+```java
     public Integer solutionPart2(String input) {
         Integer i = 0;
         while (true) {
@@ -53,4 +110,7 @@ public class TheIdealStockingStuffer extends AoCDay {
             i++;
         }
     }
+```""";
+    }
+
 }
